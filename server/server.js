1918c64d6f9419e4b37d108efcf32c75656ad76b
yulduz <yulduz83@gmail.com>
2021-05-11 22:44:12 -0500

    adds mailer functionality

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
-const express = require('express');
+const express = require("express");
+
 // const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
+const path = require("path");
+const { send } = require("process");
 
 // const { typeDefs, resolvers } = require('./schemas');
 // const { authMiddleware } = require('./utils/auth');
-const db = require('./config/connection');
+const db = require("./config/connection");
+
+const { sendEmail, EMAIL_TEMPLATE } = require("./utils/mailer");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -19,18 +23,32 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Serve up static assets
-// app.use('/images', express.static(path.join(__dirname, '../client/public/images')));
+const dummyData = {
+  name: "Yulduz",
+  lastName: "Test",
+  email: "yulduz83@gmail.com",
+  reservationDetails: {
+    date: "11/14/2021",
+    address: "123 Pine Street, NY, 12345",
+  },
+};
+sendEmail(EMAIL_TEMPLATE.BOOKING_CONFIRMATION_CONSUMER, dummyData);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+// Serve up static assets
+// app.use(
+//   "/images",
+//   express.static(path.join(__dirname, "../client/public/images"))
+// );
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);

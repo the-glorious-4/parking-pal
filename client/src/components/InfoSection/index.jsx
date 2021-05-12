a5299e950fa0fd983809ff24c6d66a63075f37c5
benwade91 <70988204+benn925@users.noreply.github.com>
2021-05-11 18:51:54 -0700

    animations for info cards

--- client/src/components/InfoSection/index.jsx
+++ client/src/components/InfoSection/index.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import './style.scss'
+import './main'
 
 const InfoSection = () => {
+
     return (<>
         <div id='infoSection' className='infoSection'>
-            <div className='infoBody'>
-                <div className='info1'>
+            <div id='infoBody'>
+                <div id='info1' className='info1'>
                     <h2>Find a spot</h2>
                 </div>
-                <div className='info2'>
+                <div id='info2'>
                     <h2>Reserve a spot</h2>
                 </div>
-                <div className='info3'>
+                <div id='info3'>
                     <h2>Stop Stressing!</h2>
                 </div>
             </div>
---
+++ client/src/components/InfoSection/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('scroll', () => {
+    document.getElementById('infoBody').className ='infoBody';
+    document.getElementById('info1').className ='boxGrow info1';
+    document.getElementById('info2').className ='boxGrow info2';
+    document.getElementById('info3').className ='boxGrow info3';
+})

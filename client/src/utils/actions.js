edfab59a4f42cd2518a8ef7e592fda02c542d0ed
Akram Sabbah <akramsabbah9@gmail.com>
2021-05-12 18:16:49 -0700

    added actions and reducers for modal

---
+++ client/src/utils/actions.js
@@ -0,0 +1,3 @@
+/* actions.js: define state-changing actions */
+export const RENDER_LOGIN_MODAL = "RENDER_LOGIN_MODAL";
+export const RENDER_SIGNUP_MODAL = "RENDER_SIGNUP_MODAL";
---
+++ client/src/utils/reducers.js
@@ -0,0 +1,33 @@
+import { useReducer } from "react";
+import {
+    RENDER_LOGIN_MODAL,
+    RENDER_SIGNUP_MODAL
+} from "./actions";
+
+/*
+state: {
+    loginRendered: boolean, used to conditionally render <LoginModal> component
+    signupRendered: boolean, used to conditionally render <SignupModal> component
+}
+*/
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case RENDER_LOGIN_MODAL:
+            return {
+                ...state,
+                loginRendered: true
+            };
+        case RENDER_SIGNUP_MODAL:
+            return {
+                ...state,
+                loginRendered: true
+            };
+        default:
+            return state;
+    }
+};
+
+export function useModalReducer(initialState) {
+    return useReducer(reducer, initialState);
+};

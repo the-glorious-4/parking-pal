745c849dbcab8b5da4f6706d4b5c8e9fac4e55e8
Akram Sabbah <akramsabbah9@gmail.com>
2021-05-14 00:28:57 -0700

    added ApolloProvider

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { StoreProvider } from "./utils/GlobalState";
 
 import Home from './pages/Home'
 
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ""
+      }
+    });
+  },
+  uri: "/graphql"
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
       <Router>
         <div>
             <StoreProvider>
@@ -19,6 +34,7 @@ function App() {
             </StoreProvider>
         </div>
       </Router>
+    </ApolloProvider>
   );
 }
 

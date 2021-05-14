fad613675b16dfde6e8f1e5e673b911a811240cd
Akram Sabbah <akramsabbah9@gmail.com>
2021-05-13 23:25:48 -0700

    added handleBlur and began validation

--- client/src/components/SignupModal/index.jsx
+++ client/src/components/SignupModal/index.jsx
@@ -2,24 +2,48 @@
 import React, { useState } from "react";
 import "./style.scss";
 import Modal from "../Modal";
+//import { formatPhoneNumber } from "../../utils/helpers";
 
 // render signup page wrapped in a Modal.
 const SignupModal = () => {
-    const [formState, setFormState] = useState({ firstname: "", lastname: "", email: "", password: "" });
+    const [formState, setFormState] = useState({ firstName: "", lastName: "", email: "", password: "", phone: "" });
+    const [errFlags, setErrFlags] = useState({ emailError: false, passLengthError: false, phoneError: false });
 
     const handleChange = event => {
+        // destructure event target
         const { name, value } = event.target;
-        setFormState({
-            ...formState,
-            [name]: value
-        });
+        // update state
+        setFormState({ ...formState, [name]: value });
     };
 
-    // TODO: validate function to check for any constraints
+    // validate form and set error messages.
+    const validateForm = fieldName => {
+        switch(fieldName) {
+            case "password":
+                setErrFlags({
+                    ...errFlags,
+                    passLengthError: (formState.password.length < 6) ? true : false,
+                });
+                break;
+            case "email":
+                // setErrFlags({
+                //     ...errFlags
+                // });
+                break;
+            default:
+                return;
+        }
+    };
+
+    // on blur, validate fields
+    const handleBlur = event => {
+        validateForm(event.target.name);
+    };
 
     const handleFormSubmit = event => {
         event.preventDefault();
-        // TODO: validate, await response from backend, get token, and login
+        validateForm();
+        // TODO: if no errors, await response from backend, get token, and login
     };
 
     return (
@@ -55,6 +79,7 @@ const SignupModal = () => {
                             type="email"
                             id="email"
                             onChange={handleChange}
+                            onBlur={handleBlur}
                         />
                     </div>
                     <div className="field">
@@ -65,12 +90,26 @@ const SignupModal = () => {
                             type="password"
                             id="pwd"
                             onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                        {!errFlags.passLengthError ? null :
+                        <span className="signup-form-err">Your password must be at least 6 characters long.</span>}
+                    </div>
+                    <div className="field">
+                        <label htmlFor="phone">Phone Number:</label>
+                        <input
+                            placeholder="555-555-5555"
+                            name="phone"
+                            type="tel"
+                            id="phone"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
                         />
                     </div>
-                    <div className="submit">
-                        <button type="submit">
-                            Submit
-                        </button>
+                    <div className="signup-submit">
+                        <div>
+                        <button type="submit">Submit</button>
+                        </div>
                     </div>
                 </form>
             </div>

8bae57f60947e7765efaf7fb5b40128fa625493d
Akram Sabbah <akramsabbah9@gmail.com>
2021-05-16 16:44:32 -0700

    route user to dashboard when logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import { StoreProvider } from "./utils/GlobalState";
+import Auth from "./utils/auth";
 
-import Home from './pages/Home'
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 
 const client = new ApolloClient({
   request: operation => {
@@ -26,7 +28,12 @@ function App() {
         <div>
             <StoreProvider>
               <Switch>
-                <Route exact path="/" component={Home} />
+                <Route exact path="/" component={Home}>
+                  {Auth.loggedIn() ? <Redirect to="/dashboard" /> : null}
+                </Route>
+                <Route exact path="/dashboard" component={Dashboard}>
+                  {!Auth.loggedIn() ? <Redirect to="/" /> : null}
+                </Route>
                 {/* <Route exact path="/login" component={Login} />
                 <Route exact path="/signup" component={Signup} />
                 <Route component={NoMatch} /> */}

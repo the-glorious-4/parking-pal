b9e048af2a02b0a17dfa485199092f5c6b103b87
Akram Sabbah <akramsabbah9@gmail.com>
2021-05-17 01:10:30 -0700

    refactored dashboard and added buttons

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -14,39 +14,43 @@ const Dashboard = () => {
     if (loading) {
         return <h1>Loading...</h1>;
     }
+    if (!Auth.loggedIn) {
+        return <h1>Returning to Homepage...</h1>;
+    }
 
     return (
         <>
-            {Auth.loggedIn() ? (
-                <>
-                    <Nav />
-                    <div className="dashboard">
-                        <h1>Welcome, {user.firstName}!</h1>
-                        <div className="active-lists">
-                            <div className="dash-list">
-                                <h2>Current and Upcoming Reservations</h2>
-                                <ul>
-                                    {/* TODO: query all reservations that list this user as a consumer */}
-                                </ul>
-                            </div>
-                            <div className="dash-list">
-                                <h2>Your Spaces</h2>
-                                <ul>
-                                    {user.parkingPlace.map(({ _id, street, city, state, zip }) => (
-                                        <li key={_id}>
-                                            {/* TODO: Link to detail view */}
-                                            {`${street} ${city}, ${state} ${zip}`}
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
+            <Nav />
+            <div className="dashboard">
+                <h1>Welcome, {user.firstName}!</h1>
+                <div className="active-lists">
+                    <div className="dash-list">
+                        <h2>Current and Upcoming Reservations</h2>
+                        <ul>
+                            {/* TODO: query all reservations that list this user as a consumer */}
+                        </ul>
+                        <button>Reserve a Parking Space</button>
                     </div>
-                </>
-            ) : (
-                <h2>Returning to Homepage...</h2>
-                // automatic redirect in App.js's router
-            )}
+                    <div className="dash-list">
+                        <h2>Your Spaces</h2>
+                        {
+                            // if user is a provider, display parking spaces they own
+                            user.parkingPlace ?
+                            <ul>
+                                {user.parkingPlace.map(({ _id, street, city, state, zip }) => (
+                                    <li key={_id}>
+                                        {/* TODO: Link to detail view. Endpoint: `/${space._id}` */}
+                                        {`${street} ${city}, ${state} ${zip}`}
+                                    </li>
+                                ))}
+                            </ul>
+                            :
+                            <span>You are currently not a Parking Space Provider.</span>
+                        }
+                        <button>Add a new Parking Space</button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };

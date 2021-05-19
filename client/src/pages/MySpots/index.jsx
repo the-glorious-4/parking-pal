263bb6a1ce1592b2ef29b744e5e9fae5307d1ccd
Akram Sabbah <akramsabbah9@gmail.com>
2021-05-19 14:07:05 -0700

    display user's parking spots

--- client/src/pages/MySpots/index.jsx
+++ client/src/pages/MySpots/index.jsx
@@ -1,13 +1,58 @@
 import React, { useState } from "react";
 import "./style.scss";
-import { useMutation } from "@apollo/react-hooks";
-import Nav from '../../components/Nav';
+import { useQuery, useLazyQuery, useMutation } from "@apollo/react-hooks";
+import { Link } from "react-router-dom";
+import Nav from "../../components/Nav";
+import Auth from "../../utils/auth";
+import { QUERY_USER,  } from "../../utils/queries";
 import { ADD_INVENTORY } from "../../utils/mutations";
 
 const MySpots = () => {
+    const { loading, data } = useQuery(QUERY_USER);
+    let spaces;
+
+    spaces = data?.user.parkingPlace || {};
+
+    if (loading) return <h1>Loading...</h1>;
+    if (!Auth.loggedIn) return <h1>Returning to Homepage...</h1>;
+
+    function renderInfo(event) {
+        event.preventDefault();
+        console.log(event.target);
+    }
+
+    console.log(spaces);
+
+
     return (<>
         <div className="my-parking-bg content-container">
             <Nav />
+            <div className="my-parking">
+                <h1>My Spaces</h1>
+                <div className="inventory-main">
+                    {/* Parking Space List */}
+                    <div className="spaces-container">
+                        <p>Select a space to view and edit its availability information.</p>
+                        {spaces ?
+                        <ul className="space-list">
+                            {spaces.map(({ _id, street, apt, city, state, zip }) => (
+                                <li key={_id} data-id={_id} onClick={renderInfo}>
+                                    {`${street} ${apt} ${city}, ${state} ${zip}`}
+                                </li>
+                            ))}
+                        </ul>
+                        :
+                        <span className="dashboard-nolist">
+                                You are currently not a Parking Space Provider.
+                        </span>}
+                        <Link to="/addparking"><button>Add a new Parking Space</button></Link>
+                    </div>
+                    {/* Scheduling calendar */}
+                    <div className="inventory-container">
+                        {/*  */}
+                    </div>
+                </div>
+            </div>
         </div>
     </>);
 };

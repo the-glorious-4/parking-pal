e0676563e1535b9361a2cf9cc465d6eea7c20915
benwade91 <70988204+benn925@users.noreply.github.com>
2021-05-18 21:12:38 -0700

    map renders plots

--- client/src/components/Map/index.jsx
+++ client/src/components/Map/index.jsx
@@ -1,16 +1,16 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
     GoogleMap,
     Marker,
     InfoWindow
 } from '@react-google-maps/api';
-import { features } from '../../utils/dummyData.json';
+import { getAllParking } from '../../utils/dummyData.json';
 import './style.scss';
 import Search from '../SearchInput'
 import FindMeBtn from '../FindMeBtn'
 import prkingLogo from './images/mapPic.png'
 import { useStoreContext } from '../../utils/GlobalState';
-
+import { UPDATE_MAP_LOCATION } from '../../utils/actions';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_ALL_PARKING } from "../../utils/queries";
 // import { getGeocode, getLatLng } from 'use-places-autocomplete';
@@ -25,36 +25,44 @@ const options = {
     zoomControl: true
 }
 
-const plots = features.map(location => (location.geometry.coordinates))
+const plots = getAllParking.map(location => (location))
 
 function MyMapComponent(props) {
 
 
-    const [state,] = useStoreContext();
-    const [markers,] = useState(plots)
+    const [state, dispatch] = useStoreContext();
+    const [markers, setMarkers] = useState(plots)
     const [, setMap] = useState(null)
     const [selected, setSelected] = useState(null)
-    const mapRef = useRef();
 
-    const onLoad = (map) => {
-        mapRef.current = map;
-        setMap(mapRef)
-    };
+    // const mapRef = useRef();
+
+    // const onLoad = (map) => {
+    //     mapRef.current = map;
+    //     setMap(mapRef)
+    // };
 
     const parkingRedirect = () => {
-        console.log(features.filter(marker => marker.geometry.coordinates === selected));
+        console.log(markers.filter(marker => marker.parkingPlace.latLng === selected));
     }
 
     const { loading, data } = useQuery(QUERY_ALL_PARKING,
         { variables: { city: state.mapCity, startDate: state.mapDate } },
     );
     if (loading) {
-        // console.log('loading');
-    } 
-    
-    if(data){
-        console.log(data.getAllParking);
+        console.log('loading');
     }
+    // console.log(selected);
+
+    useEffect(() => {
+
+        if (data) {
+            console.log(data.getAllParking);
+            setMarkers(data.getAllParking);
+        }
+
+    }, [data])
+console.log(markers);
 
     return (
         <div className='mapBody'>
@@ -72,9 +80,11 @@ function MyMapComponent(props) {
                 zoom={15}
                 center={state.mapLocation ? state.mapLocation : { lat: 37.774, lng: -122.419 }}
                 options={options}
-                onLoad={onLoad}
+            // onLoad={onLoad}
             >
-                {markers.map(marker => <Marker
+               { markers &&  
+               (markers.map(marker => <Marker
+               
                     icon={{
                         url: prkingLogo,
                         scaledSize: new window.google.maps.Size(40, 40),
@@ -82,18 +92,23 @@ function MyMapComponent(props) {
                         anchor: new window.google.maps.Point(20, 20)
                     }}
                     key={markers.indexOf(marker)}
-                    onClick={() => { setSelected(marker) }}
-                    position={{ lat: marker[1], lng: marker[0] }}
+                    onClick={() => {
+                        setSelected(marker)
+                        dispatch({
+                            type: UPDATE_MAP_LOCATION,
+                            location: { lat: parseFloat(marker.parkingPlace.latLng[1]), lng: parseFloat(marker.parkingPlace.latLng[0]) }
+                        })
+                    }}
+                    position={{ lat: parseFloat(marker.parkingPlace.latLng[1]), lng: parseFloat(marker.parkingPlace.latLng[0]) }}
                     id={markers.indexOf(marker)}
-                />)}
+                    onLoad={console.log(marker, 'heres the marker')}
+                />))}
 
                 {selected ? (
                     <InfoWindow
-                        position={{ lat: selected[1], lng: selected[0] }}
+                        position={{ lat: parseFloat(selected.parkingPlace.latLng[1]), lng: parseFloat(selected.parkingPlace.latLng[0]) }}
                         onCloseClick={() => { setSelected(null) }}
                     >
-
-                        {/* THIS IS THE MAP BUBBLE FILLER */}
                         <div>
                             <h3 style={{ textAlign: 'center' }}>Parking</h3>
                             <p>This is a pretty great spot</p>

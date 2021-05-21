afb666fd9cb673674cc13b91f3edb64b9c92b7c7
Bhumisha Dave <bhumishadave@yahoo.com>
2021-05-20 17:54:46 -0700

    Updated service-worker forced to commit all other files

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './service-worker';
-// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.Fragment>
@@ -17,4 +15,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
  reportWebVitals();
-serviceWorker.register();
+ serviceWorkerRegistration.register();
